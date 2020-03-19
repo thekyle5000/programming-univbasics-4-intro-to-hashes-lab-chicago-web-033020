638da28d628876cb385d81e618a5cfa8341a09ce
Kyle Bradley <kylepersonalmail@gmail.com>
2020-03-19 03:35:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,8 @@ end
 def update_counting_hash(hash, key)
   if hash[key] 
     hash[key] +=1
-  else hash[key] = 1
+  else 
+    hash[key] = 1
   end
   
   # given a hash an a key as parameters, return an updated hash
